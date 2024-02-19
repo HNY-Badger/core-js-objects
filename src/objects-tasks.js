a61a7409c22550cd9b4fe0dc9694eb541cf5317f
HNY_Badger <markelov-02@list.ru>
2024-02-19 23:14:14 +0400

    feat: makeWord

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -127,8 +127,20 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const positionsObject = {};
+  const lettersKeys = Object.keys(lettersObject);
+  const lettersValues = Object.values(lettersObject);
+  for (let i = 0; i < lettersValues.length; i += 1) {
+    lettersValues[i].forEach((pos) => {
+      positionsObject[pos] = lettersKeys[i];
+    });
+  }
+  let word = '';
+  for (let i = 0; i < Object.keys(positionsObject).length; i += 1) {
+    word += positionsObject[i];
+  }
+  return word;
 }
 
 /**
